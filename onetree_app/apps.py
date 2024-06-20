d0a7f5a35d229ba931bfdcf0e7a7ce458389ed91
kabin <ghimirekabin060@gmail.com>
2024-06-20 20:49:17 +0545

    Project set done!!

---
+++ onetree_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OnetreeAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'onetree_app'
---
+++ onetree_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+
+def menu(request):
+    return render(request,'menu.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
